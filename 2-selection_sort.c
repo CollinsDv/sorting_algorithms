c6e5412d7780b7850bc7a6ad8a0449d169e053b3
Daniel Nyakundi <nyaxda@gmail.com>
2024-02-17 07:56:53 -0800

    Fix selection sort algorithm to correctly find minimum index

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,15 +10,18 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int temp;
+	int temp, min_index;
 
 	if (array == NULL || size < 2)
 		return;
 	for (i = 0; i < size - 1; i++)
 	{
+		min_index = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[i])
+			if (array[j] < array[min_index])
+				min_index = j;
+			if (min_index != i)
 			{
 				temp = array[i];
 				array[i] = array[j];
