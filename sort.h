e25cdb412a692626a354ca924ee27ac0956da302
Daniel Nyakundi <nyaxda@gmail.com>
2024-02-17 05:11:10 -0800

    Pushed preliminary fils

---
+++ sort.h
@@ -0,0 +1,14 @@
+#ifndef SORT_H
+#define SORT_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+#endif
