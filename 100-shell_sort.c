79077b350ae0ccee4d50a31124eda0ac8ee39e0a
Daniel Nyakundi <nyaxda@gmail.com>
2024-02-19 06:05:33 -0800

    Refactor shell_sort function to improve interval calculation

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -9,10 +9,14 @@
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t interval, i, j;
+	size_t interval = 1, i, j;
 	int temp;
 
-	for (interval = size / 3; interval >= 1; interval = (interval - 1) / 3)
+	while (interval < size / 3)
+	{
+		interval = interval * 3 + 1;
+	}
+	for (; interval >= 1; interval /= 3)
 	{
 		for (i = interval; i < size; i++)
 		{
