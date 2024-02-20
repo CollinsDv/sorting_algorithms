0ecd6adfc722fc92c9c2aa256fc62eeaaa6c800a
Daniel Nyakundi <nyaxda@gmail.com>
2024-02-20 06:16:19 -0800

    Fix partition function in quicksort algorithm

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -54,6 +54,8 @@ void partition(int *arr, size_t size, int left, int right)
 		swap(&arr[i], &arr[j]);
 		print_array(arr, size);
 	}
+	partition(arr, size, left, j);
+    partition(arr, size, j + 1, right);
 }
 
 /**
